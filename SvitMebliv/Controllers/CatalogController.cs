c73097403cecd1763911150d1c4a493638dec84d
Microsoft <Microsoft@DESKTOP-HE34PRB>
2017-01-15 19:38:23 -0800

    Add project files.

---
+++ SvitMebliv/Controllers/CatalogController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SvitMebliv.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public ActionResult Okompanii()
+        {
+            return View();
+        }
+
+        public ActionResult Uslugi()
+        {
+            return View();
+        }
+
+        public ActionResult Publitations()
+        {
+            return View();
+        }
+
+        public ActionResult Akcii()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakty()
+        {
+            return View();
+        }
+    }
+}
